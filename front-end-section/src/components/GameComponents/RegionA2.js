290d09ee6b9e61c4055bf2a4cb4a72271b9cf311
dhackelman <drew.hackelman@gmail.com>
2017-03-13 13:03:22 -0400

    Add Region Components to react folders

---
+++ front-end-section/src/components/GameComponents/RegionA2.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class RegionA2 extends React.Component {
+
+    render() {
+        return (
+            <div className="region__A">
+
+                <h1>Region A2</h1>
+
+            </div>
+        );
+    }
+}
+
+export default RegionA2;
