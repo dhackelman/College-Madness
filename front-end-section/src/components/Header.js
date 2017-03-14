4f3a3e28d252aaf1158ab0941b7c897aecec336f
Shane <shanemccoypdx@gmail.com>
2017-03-13 22:25:59 -0400

    header styling and image added plus fontawesome

--- front-end-section/src/components/Header.js
+++ front-end-section/src/components/Header.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import FaGraduationCap from 'react-icons/lib/fa/graduation-cap';
 
 class Header extends React.Component {
 
     render() {
         return (
             <div className="header__container">
-                <div className="header__logo">logo</div>
-                <div className="header__homerooom">homeroom</div>
-                <span className="header__logout kippBtn">Logout</span>
+                <img className="header__logo" src={require('../styles/images/KIPP-Logo.png')} alt={"KIPP Logo"}/>
+                <div className="header__homerooom">
 
+                    <h1><FaGraduationCap size={50}/>
+                        Anders</h1>
+                </div>
+                <span className="header__logout kippBtn">Logout</span>
             </div>
         );
     }
