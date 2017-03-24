0bd7001bd98dec2b30d5efa2b4fbcb10c485700c
dhackelman <drew.hackelman@gmail.com>
2017-03-24 14:26:33 -0400

    add teams 4 and 8 and 2

--- src/Data/Dummy8.js
+++ src/Data/Dummy8.js
@@ -35,6 +35,42 @@ const Data = {
             image: "../styles/images/teamLogo.png"
 
         }
+    }, {
+        id: 1,
+        predicted_wins: 0,
+        team: {
+            name: "",
+            abbrev: "4",
+            image: "../styles/images/teamLogo.png"
+
+        }
+    }, {
+        id: 1,
+        predicted_wins: 0,
+        team: {
+            name: "",
+            abbrev: "4",
+            image: "../styles/images/teamLogo.png"
+
+        }
+    }, {
+        id: 1,
+        predicted_wins: 0,
+        team: {
+            name: "",
+            abbrev: "4",
+            image: "../styles/images/teamLogo.png"
+
+        }
+    }, {
+        id: 1,
+        predicted_wins: 0,
+        team: {
+            name: "",
+            abbrev: "4",
+            image: "../styles/images/teamLogo.png"
+
+        }
     }]
 }
 
