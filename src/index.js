bd647462eb8a8e6220e8ad09d988b0460721e5d6
dhackelman <drew.hackelman@gmail.com>
2017-03-25 14:30:25 -0400

    fixed student

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import Login from './components/Login';
 import TeacherConfirm from './components/TeacherConfirm';
 import TeacherLogin from './components/TeacherComponents/Teacher';
 import SelectClass from './components/SelectClass';
-import StudentLogin from './components/TeacherComponents/Student';
 import MainBracket from './components/GameComponents/MainBracket';
 import ResearchContainer from './components/ResearchComponents/ResearchContainer';
 import ResearchAcademicStats from './components/ResearchComponents/ResearchAcademicStats';
@@ -23,7 +22,6 @@ ReactDOM.render((
             <Route path='teacher' component={TeacherLogin}/>
             <Route path='teacher-confirm/:id' component={TeacherConfirm}/>
             <Route path='select-class/:user' component={SelectClass}/>
-            <Route path='student' component={StudentLogin}/>
             <Route path='teacher-confirm' component={TeacherConfirm}/>
             <Route path='research/:id' component={ResearchContainer}/>
             <Route path='researchAcademics' component={ResearchAcademicStats}/>
