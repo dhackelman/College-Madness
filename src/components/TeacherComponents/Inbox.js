478d441f69125dfea4d809bad955043988d40060
dhackelman <drew.hackelman@gmail.com>
2017-03-25 19:29:29 -0400

    Working on inbox population

--- src/components/TeacherComponents/Inbox.js
+++ src/components/TeacherComponents/Inbox.js
@@ -5,37 +5,51 @@ import axios from 'axios';
 class Inbox extends React.Component {
   constructor() {
     super();
+    this.iterateStudentInput = this.iterateStudentInput.bind(this);
     this.state = {
-      Messages: []
+      Messages: [],
+      ActualMessages: []
     };
-    this.unfurlMessages = this.unfurlMessages.bind(this);
   }
-  componentDidMount() {
+  componentWillMount() {
       axios.get("https://kipp-madness-api.herokuapp.com/classrooms/1.json")
         .then((inbox) =>{
-          this.unfurlMessages(inbox.data.students);
+          this.setState({Messages:inbox.data.students});
         });
+
     }
 
-  unfurlMessages(arg) {
-    let usermessages = arg;
-    let messageArray = [];
-    for (let i = 0; i < arg.length; i++){
-      console.log(arg[i].submissions);
-      for (let j = 0; j < arg[i].submissions.length; j++) {
-        messageArray.push(arg[i].submissions[j]);
-        }
+  iterateStudentInput(arg) {
+    let messageInnards = arg;
+    let butts = [];
+    for (let i=0; i < messageInnards.length; i++) {
+      let obj = messageInnards[i];
+      butts.push(Object.keys(obj).map(key => obj[key]));
       }
-    console.log(messageArray);
-    this.setState({Messages:messageArray});
-  }
+      console.log(butts);
 
+      // console.log('init', arr);
+      // arr[2].map((stud)=> {
+      //   let studObj = stud;
+      //   let studArr = Object.keys(studObj).map(key => studObj[key]);
+      //   let actualMessages = studArr[3];
+      //   console.log(actualMessages);
+      //   });
+      // });
+    }
+
+  // for (let i=0; i < messageInnards.length; i++) {
+  //   var arg1 = messageInnards[i];
+  //   var argArray = Object.keys(arg1).map((key) => {
+  //       allmessages.push(arg1[key]);
+  //     });
+  //     console.log("after iter", allmessages);
 
     render() {
         return (
           <div className="teacher__inbox">
             <p className="inbox__header">Room 2105</p>
-            <Message messages={this.state.Messages}/>
+            <Message messages={this.state.Messages} iterate={this.iterateStudentInput}/>
           </div>
         );
     }
--- src/components/TeacherComponents/Message.js
+++ src/components/TeacherComponents/Message.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 class Message extends React.Component {
 
+
     render() {
         return (
           <ul className="inbox__messages">
-          {this.props.messages.map((user)=>{
-            return <li className="inbox__single" key={user.submission_id}><p>{user.input}</p><button className="inbox__pass">Pass</button><button className="inbox__fail">Fail</button></li>
-            })}
+            {this.props.iterate(this.props.messages)}
+                <button className="inbox__pass">Pass</button>
+                <button className="inbox__fail">Fail</button>
           </ul>
         );
     }
