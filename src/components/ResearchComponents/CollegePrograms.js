d59c1c8d96b19c7443bc00f75208144cf5d8ebf1
dhackelman <drew.hackelman@gmail.com>
2017-03-25 10:50:10 -0400

    working Link to outside school site

--- src/components/ResearchComponents/CollegePrograms.js
+++ src/components/ResearchComponents/CollegePrograms.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 class CollegePrograms extends React.Component {
 
 
 
+
     render() {
-      const school = this.props.school;
+
+      const site = "http://" + this.props.school.url;
         return (
                 <div className="college__basics">
-                    books and shit! <a href={this.props.school.url}></a>
+                    books and shit! <Link to={site} target="_blank" alt={this.props.school.name}>{this.props.school.url}</Link>
                 </div>
         );
     }
