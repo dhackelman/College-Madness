0d2ed9367276a1f948fe796d5c529a47001c920f
dhackelman <drew.hackelman@gmail.com>
2017-03-28 14:46:22 -0400

    disable homeroom link temp

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router';
 import FaGraduationCap from 'react-icons/lib/fa/graduation-cap';
 
 class Header extends React.Component {
-
+  // <h1><Link to="/teacher"><FaGraduationCap size={50}/>
+  //     My Homeroom</Link></h1>
     render() {
         return (
             <div className="header__container">
                 <img className="header__logo" src={require('../styles/images/KIPP-Logo.png')} alt={"KIPP Logo"}/>
                 <div className="header__homerooom">
-                    <h1><Link to="/teacher"><FaGraduationCap size={50}/>
-                        My Homeroom</Link></h1>
+                  <h1><FaGraduationCap size={50}/>My Homeroom</h1>
                 </div>
                 <span className="header__logout kippBtn"><Link to="/">Logout</Link></span>
             </div>
