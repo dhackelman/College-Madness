de2418efa36372fd47edaf1d3deb5adc637b6bb0
Shane <shanemccoypdx@gmail.com>
2017-03-28 14:01:29 -0400

    ternary

--- src/components/GameComponents/FinalTeam.js
+++ src/components/GameComponents/FinalTeam.js
@@ -9,7 +9,7 @@ class FinalTeam extends React.Component {
         return (
           <div className="team__container" onClick={()=> {this.props.selectFour(this.props.home.team.id, this.props.away.team.id, this.props.round)}}>
             <Link to={collegeSpecificResearch}><FaInfoCircle className="team__info" size={30}/></Link>
-            <img className="team__logo" src={this.props.home.team.image ? this.props.home.team.image : "https://s-media-cache-ak0.pinimg.com/236x/7c/aa/e9/7caae9c662662230b13d53033e4dbbfa.jpg"} alt={this.props.home.team.abbrev}></img>
+            <img className="team__logo" src={this.props.home.team.image ? this.props.home.team.image : "https://kipp-madness.herokuapp.com/bball-placeholder.png"} alt={this.props.home.team.abbrev}></img>
             <span className="team__seed">{this.props.home.team.seed ? this.props.home.team.seed : ''}</span>
             <span className="team__name">{this.props.home.team.abbrev}</span>
           </div>
