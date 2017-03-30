b238e4db4555522e5124709710b38725f0d44e58
dhackelman <drew.hackelman@gmail.com>
2017-03-30 10:44:34 -0400

    Updated ternary on the icon for F4 and Champ

--- src/components/GameComponents/FinalTeam.js
+++ src/components/GameComponents/FinalTeam.js
@@ -8,7 +8,7 @@ class FinalTeam extends React.Component {
       const collegeSpecificResearch = "research/" + this.props.home.team.id;
         return (
           <div className="team__container" onClick={()=> {this.props.selectFour(this.props.home.team.id, this.props.away.team.id, this.props.round)}}>
-            <Link to={collegeSpecificResearch}><FaInfoCircle className="team__info" size={30}/></Link>
+            <Link to={collegeSpecificResearch}><FaInfoCircle className="team__info" size={30} className={!this.props.home.predicted_wins && this.props.round !== 'round_64' ? "is-hidden" : "team__info"}/></Link>
             <img className={!this.props.home.team.image ? "is-hidden" : "team__logo"} src={this.props.home.team.image} alt={this.props.home.team.name}></img>
             <img className={this.props.home.team.image ? "is-hidden" : "team__logo placeholder"}  src="https://kipp-madness.herokuapp.com/bball-placeholder.png" alt='basketball'></img>
             <span className="team__seed">{this.props.home.team.seed ? this.props.home.team.seed : ''}</span>
