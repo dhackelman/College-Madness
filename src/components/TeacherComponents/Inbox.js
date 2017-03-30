a044eed436347581475825716aee0635f9ddf5a1
dhackelman <drew.hackelman@gmail.com>
2017-03-30 14:29:01 -0400

    working inbox stuff

--- src/components/TeacherComponents/Inbox.js
+++ src/components/TeacherComponents/Inbox.js
@@ -11,14 +11,15 @@ class Inbox extends React.Component {
               {this.props.submssions.map((thing)=> {
                 console.log(thing);
                 return <li className="inbox__single" key={thing.submission_id}>
+                  <div className={thing.status ?  "overlay" :"transparent"}>
+                    <p className={thing.status ?  "p" :"is-hidden"}>Feedback Given.</p>
+                  </div>
                   <p>{thing.input}</p>
                   <div className="button__holder">
                     <button onClick={()=>{this.props.updateSubmssionScore(true, thing.submission_id)}} className={thing.submission_id === 0 ? "is-hidden" : "inbox__pass kippBtn"}>Great Job!</button>
                     <button onClick={()=>{this.props.updateSubmssionScore(false, thing.submission_id)}} className={thing.submission_id === 0 ? "is-hidden" : "inbox__fail kippBtn"}>Try Again</button>
                   </div>
-                  <div className={!thing.status ?  "overlay" :"transparent"}>
-                    <p className={!thing.status ?  "p" :"is-hidden"}>Feedback Given.</p>
-                  </div>
+
                 </li>
               })}
             </ul>
