d7c222d53906d7b86548b7e824efeb4ec86b8e62
dhackelman <drew.hackelman@gmail.com>
2017-03-30 13:44:11 -0400

    working on inbox filter again

--- src/components/TeacherComponents/Inbox.js
+++ src/components/TeacherComponents/Inbox.js
@@ -15,10 +15,13 @@ class Inbox extends React.Component {
                   <div className="button__holder">
                     <button onClick={()=>{this.props.updateSubmssionScore(true, thing.submission_id)}} className={thing.submission_id === 0 ? "is-hidden" : "inbox__pass kippBtn"}>Great Job!</button>
                     <button onClick={()=>{this.props.updateSubmssionScore(false, thing.submission_id)}} className={thing.submission_id === 0 ? "is-hidden" : "inbox__fail kippBtn"}>Try Again</button>
-                  </div>                  
+                  </div>
                 </li>
               })}
             </ul>
+            <div className="overlay">
+              <img src={req('http://college-madness.herokuapp.com/processed.png')}</img>
+            </div>
         </div>
         );
     }
